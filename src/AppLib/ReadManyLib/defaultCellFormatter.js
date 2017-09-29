320c46bf424f58e5d7c0ff581b2b11ca624068ce
Chun-Yang <yang2007chun@gmail.com>
2017-09-29 14:05:06 -0500

    update ui

--- src/AppLib/ReadManyLib/defaultCellFormatter.js
+++ src/AppLib/ReadManyLib/defaultCellFormatter.js
@@ -1,9 +1,5 @@
-import React from 'react'
-
 function defaultCellFormatter (value) {
-  return <pre>
-    {JSON.stringify(value, null, 2)}
-  </pre>
+  return value
 }
 
 export default defaultCellFormatter
