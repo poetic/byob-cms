098c244a9b70b15a09334235a16a7dcac780b99a
Chun-Yang <yang2007chun@gmail.com>
2017-10-25 15:49:02 -0500

    use null to replace empty object

--- src/formLib/undefinedToNull.js
+++ src/formLib/undefinedToNull.js
@@ -5,6 +5,10 @@ function undefinedToNull (data) {
     // remove null in array
     return data.filter((value) => value != null)
   } else if (typeof data === 'object') {
+    // use null to replace empty object
+    if (Object.keys(data).length === 0) {
+      return null
+    }
     return Object.keys(data).reduce((acc, key) => {
       return {
         ...acc,
