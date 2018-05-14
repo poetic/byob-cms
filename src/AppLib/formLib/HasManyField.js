3ea949ad7ce7b496147afa345c9297176456ceb8
Francisco Sales <franciscofsales1@gmail.com>
2018-05-14 23:19:41 +0100

    fix issues; only show sort when available

--- src/AppLib/formLib/HasManyField.js
+++ src/AppLib/formLib/HasManyField.js
@@ -19,11 +19,10 @@ class MultiSelectWidget extends React.Component {
       disabled,
       readonly,
     } = this.props
-
     const id = idSchema.$id
     const lbl = uiSchema['ui:title'] || schema.title || name
     const valueItems = (formData || []).map(item => optionItems.find(opt => opt.value === item))
-
+    const sortable = uiSchema['ui:options'] && uiSchema['ui:options'].sortable
     // NOTE: the reason we do this is to fix a bug in react selectize
     // that package caches the renderValue function and it will use the
     // up to date closure data for the function, we use a instance method
@@ -34,7 +33,29 @@ class MultiSelectWidget extends React.Component {
     }
 
     this.resortItems = (values) => {
-      onChange(values)
+      const valItem = values.map(v => formData.find(f => f === v))
+      onChange(valItem)
+    }
+
+    this.reorder = (value, direction) => {
+      let values = valueItems.map(v => v.value)
+      const indx = values.indexOf(value)
+      if (direction === '<') {
+        values = [
+          ...values.slice(0, indx - 1),
+          value,
+          values[indx - 1],
+          ...values.slice(indx + 1),
+        ]
+      } else {
+        values = [
+          ...values.slice(0, indx),
+          values[indx + 1],
+          value,
+          ...values.slice(indx + 2),
+        ]
+      }
+      this.resortItems(values)
     }
 
     return (
@@ -42,12 +63,12 @@ class MultiSelectWidget extends React.Component {
         <Label label={lbl} required={required} id={id} />
         <MultiSelect
           disabled={disabled || readonly}
-          values={valueItems}
+          values={valueItems.map((v, i) => ({ ...v, index: i }))}
           options={sortBy(optionItems, ({ label }) => label.toUpperCase())}
           onValuesChange={(v) => {
             onChange(v.map(val => val.value))
           }}
-          renderValue={item => renderValue(item, valueItems, this)}
+          renderValue={item => renderValue(item, sortable, valueItems, this)}
         />
       </div>
     )
--- src/AppLib/formLib/renderValue.js
+++ src/AppLib/formLib/renderValue.js
@@ -1,37 +1,15 @@
 import React from 'react'
 
 // add a "x" so that the user can remove that item directly
-function renderValue(item, items, parent) {
-  const reorder = (value, direction) => {
-    let values = items.map(itm => itm.value)
-    const indx = values.indexOf(value)
-    if (direction === '<') {
-      values = [
-        ...values.slice(0, indx - 1),
-        value,
-        values[indx - 1],
-        ...values.slice(indx + 1),
-      ]
-    } else {
-      values = [
-        ...values.slice(0, indx),
-        values[indx + 1],
-        value,
-        ...values.slice(indx + 2),
-      ]
-    }
-    parent.resortItems(values)
-  }
-
-  const index = items.findIndex(itm => itm.label === item.label)
+function renderValue(item, sortable, items, parent) {
   return (
     <div className="value-wrapper">
       <div className="simple-value">
-        {index > 0 &&
+        {sortable && item.index > 0 &&
           <span
             role="presentation"
             onClick={() => {
-              reorder(item.value, '<')
+              parent.reorder(item.value, '<')
             }}
           >
             {'<'}
@@ -46,11 +24,11 @@ function renderValue(item, items, parent) {
           x
         </span>
         <span>{item.label}</span>
-        {index < items.length -1 &&
+        {sortable && item.index < items.length - 1 &&
           <span
             role="presentation"
             onClick={() => {
-            reorder(item.value, '>')
+            parent.reorder(item.value, '>')
           }}
           >
             {'>'}
