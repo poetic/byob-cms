6299378f45af3c9216394ccbe6bd188ff1426432
Francisco Sales <franciscofsales1@gmail.com>
2018-05-14 22:06:41 +0100

    adding sort buttons

--- src/AppLib/formLib/HasManyField.js
+++ src/AppLib/formLib/HasManyField.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import { MultiSelect } from 'react-selectize'
+import { sortBy } from 'lodash'
 import Label from './Label'
 import renderValue from './renderValue'
 import withOptionItems from './withOptionItems'
-import { sortBy } from 'lodash'
 
 class MultiSelectWidget extends React.Component {
   render() {
@@ -22,8 +22,7 @@ class MultiSelectWidget extends React.Component {
 
     const id = idSchema.$id
     const label = uiSchema['ui:title'] || schema.title || name
-    const valueItems = optionItems
-      .filter(option => (formData || []).includes(option.value))
+    const valueItems = (formData || []).map(item => optionItems.find(opt => opt.value === item))
 
     // NOTE: the reason we do this is to fix a bug in react selectize
     // that package caches the renderValue function and it will use the
@@ -34,18 +33,24 @@ class MultiSelectWidget extends React.Component {
       onChange(nextFormData)
     }
 
-    return <div>
-      <Label label={label} required={required} id={id} />
-      <MultiSelect
-        disabled={disabled || readonly}
-        values={valueItems}
-        options={sortBy(optionItems, ({ label }) => label.toUpperCase())}
-        onValuesChange={v => {
-          onChange(v.map(v => v.value))
-        }}
-        renderValue={item => renderValue(item, this)}
-      />
-    </div>
+    this.resortItems = (values) => {
+      onChange(values)
+    }
+
+    return (
+      <div>
+        <Label label={label} required={required} id={id} />
+        <MultiSelect
+          disabled={disabled || readonly}
+          values={valueItems}
+          options={sortBy(optionItems, ({ lbl }) => lbl.toUpperCase())}
+          onValuesChange={(v) => {
+            onChange(v.map(val => val.value))
+          }}
+          renderValue={item => renderValue(item, valueItems, this)}
+        />
+      </div>
+    )
   }
 }
 
--- src/AppLib/formLib/renderValue.js
+++ src/AppLib/formLib/renderValue.js
@@ -1,17 +1,64 @@
 import React from 'react'
 
 // add a "x" so that the user can remove that item directly
-function renderValue (item, parent) {
-  return <div  className="value-wrapper">
-    <div  className="simple-value">
-      <span onClick={() => {
-        parent.removeByValue(item.value)
-      }}>
-        x
-      </span>
-      <span>{item.label}</span>
+function renderValue(item, items, parent) {
+  const reorder = (value, direction) => {
+    let values = items.map(itm => itm.value)
+    const indx = values.indexOf(value)
+    if (direction === '<') {
+      values = [
+        ...values.slice(0, indx - 1),
+        value,
+        values[indx - 1],
+        ...values.slice(indx + 1),
+      ]
+    } else {
+      values = [
+        ...values.slice(0, indx),
+        values[indx + 1],
+        value,
+        ...values.slice(indx + 2),
+      ]
+    }
+    parent.resortItems(values)
+  }
+
+  const index = items.findIndex(itm => itm.label === item.label)
+  return (
+    <div className="value-wrapper">
+      <div className="simple-value">
+        {index > 0 &&
+          <span
+            role="presentation"
+            onClick={() => {
+              reorder(item.value, '<')
+            }}
+          >
+            {'<'}
+          </span>
+        }
+        <span
+          role="presentation"
+          onClick={() => {
+            parent.removeByValue(item.value)
+          }}
+        >
+          x
+        </span>
+        <span>{item.label}</span>
+        {index < items.length -1 &&
+          <span
+            role="presentation"
+            onClick={() => {
+            reorder(item.value, '>')
+          }}
+          >
+            {'>'}
+          </span>
+        }
+      </div>
     </div>
-  </div>
+  );
 }
 
 export default renderValue
